cc98f15819b280577c6faa1f778d8b52b4cc41ed
Benjamin Bergman <benjamin.bergman@humanrights.ca>
2018-08-29 15:48:03 -0500

    Reconfigure to show failing condition by default

--- index.js
+++ index.js
@@ -4,12 +4,12 @@ const record = require('node-record-lpcm16');
 
 
 // Stream the file to be sent to the wit.ai
-//var stream = fs.createReadStream("hello.wav"); // English
-var stream = fs.createReadStream("bonjour.wav"); // French
+var stream = fs.createReadStream("hello.wav"); // English
+//var stream = fs.createReadStream("bonjour.wav"); // French
 
 // The wit.ai instance api key
-var API_KEY = "U6NOYN54EX5OVQ75R2P65HQ6JUOL4PW6"; // English
-//var API_KEY = "OFATXR4US2YKDBVHHDZZ3YWYLGWQ6QUJ"; // French
+//var API_KEY = "U6NOYN54EX5OVQ75R2P65HQ6JUOL4PW6"; // English
+var API_KEY = "OFATXR4US2YKDBVHHDZZ3YWYLGWQ6QUJ"; // French
 
 // The content-type for this audio stream (audio/wav, ...)
 var content_type = "audio/wav";
